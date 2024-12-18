fd27654b229f220eff61cce8f62a846f84006dab
TD-JamesZhang <xiaojiao.zhang@china.talkdesk.com>
2024-12-18 18:09:21 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Main = () =>
       <div className='flex-item'>
         <h1>Conversation Host</h1>
         <div className='container'>
-          <iframe sandbox='allow-scripts allow-same-origin allow-forms allow-popups allow-popups-to-escape-sandbox allow-top-navigation-to-custom-protocols allow-downloads' allow="microphone;speaker;autoplay;fullscreen;clipboard-write;camera" src='https://ccorecn.trytalkdesk.com/atlas/apps/conversation' title='conversation' />
+          <iframe sandbox='allow-scripts allow-same-origin allow-forms allow-popups allow-popups-to-escape-sandbox allow-top-navigation-to-custom-protocols allow-downloads' allow="microphone;speaker;autoplay;fullscreen;clipboard-write;camera" src='https://ccorecn.trytalkdesk.com/atlas/apps/conversation?mode=host' title='conversation' />
         </div>
       </div>
     </div>
